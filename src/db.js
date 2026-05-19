b7fc38b8252ed5abc7c3c97df2e826b7d4b9762f
dtrinquelle-a11y <dtrinquelle@gmail.com>
2026-05-19 23:51:32 +0200

    ajout db.js

---
+++ src/db.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const { createClient } = require('@supabase/supabase-js');
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+pool.connect()
+  .then(() => console.log('PostgreSQL Supabase connecté'))
+  .catch(err => console.error('Erreur DB :', err.message));
+
+module.exports = { supabase, pool };
