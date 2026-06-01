fc212492115dea63762e2b1d18aa453ca9efe723
dtrinquelle-a11y <dtrinquelle@gmail.com>
2026-06-01 21:59:19 +0200

    fix import pool employees

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
-
+const { pool } = require('../db');
 // GET /api/employees
 router.get('/', async (req, res) => {
   try {
